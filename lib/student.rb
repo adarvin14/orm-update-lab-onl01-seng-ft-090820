41ce195317c74758c4f90205185f6b4686ec0efe
Alex Darvin <amdarvin14@gmail.com>
2020-10-10 16:56:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,6 +60,6 @@ class Student
   
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    DB[:conn].execute(sql, name).map{|row| new_from_db.first
+    DB[:conn].execute(sql, name).map{|row| new_from_db(row)}.first
   
 end
